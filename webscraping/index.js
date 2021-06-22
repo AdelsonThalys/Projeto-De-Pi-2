e30e51e9fb731e4aff4f25d4762ef9f7aa184d52
AdelsonThalys <68720308+AdelsonThalys@users.noreply.github.com>
2021-06-21 23:00:48 -0300

    Update index.js

--- webscraping/index.js
+++ webscraping/index.js
@@ -1,5 +1,10 @@
-
+const axios = require('axios')
 const cheerio = require('cheerio')
+
+//substituir os espaços por + 
+var pergunta = 'como os indios chegaram no brasil'
+var perguntacorrigida = ('https://www.google.com/search?q='+pergunta+'&rlz=1C1ISCS_pt-PTBR934BR934&oq=como+os+indios+chegaram+na+america&aqs=chrome.0.0j69i57j0l8.3641j0j7&sourceid=chrome&ie=UTF-8')
+var pesquisa = perguntacorrigida.replace(/ /g, '+')
 
 const fetchData = async(url) => {
     const result = await axios.get(url)
@@ -7,7 +12,7 @@ const fetchData = async(url) => {
 }
 
 const main = async () => {
-    const content = await fetchData("https://www.google.com/search?q=como+os+indios+chegaram+na+america&rlz=1C1ISCS_pt-PTBR934BR934&oq=quem+desc&aqs=chrome.0.0j69i57j0l8.3641j0j7&sourceid=chrome&ie=UTF-8")
+    const content = await fetchData(pesquisa)
     const $ = cheerio.load(content)
 
     $('h3').each((i, e) => {
@@ -16,3 +21,4 @@ const main = async () => {
 }
 
 main()
+
