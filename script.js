caccbf8c5997139877c426fdbf02d4e4307af32e
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2021-09-11 20:20:25 -0300

    making

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function question(){
     const question = document.getElementById('question_search').value;
     alert(question)
 }
+
+document.addEventListener('keypress', function(e){
+    if(e.key == "Enter"){
+       var search = document.getElementById("question_search").value;
+       alert(search)
+    }
+ }, false);
