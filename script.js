df98189c9285fec0aaf14b3ac4189aae4369740f
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2021-09-12 18:43:15 -0300

    making 0.2

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ function question(){
 
 document.addEventListener('keypress', function(e){
     if(e.key == "Enter"){
-       var search = document.getElementById("question_search").value;
-       alert(search)
+        searching(document.getElementById("question_search").value)
+        }
+    }, false
+);
+
+const box_search = document.getElementById("search")
+function searching(question){
+    if (window.getComputedStyle(box_search).display !== "none"){
+        alert(question)
     }
- }, false);
+     
+ }
