7ea7526cfeb2703dc677059f85c3d4d20c1780c8
Luís Kennedy <62557726+KancsSneed@users.noreply.github.com>
2021-09-13 10:55:59 -0300

    Aceitavel

--- script.js
+++ script.js
@@ -12,9 +12,25 @@ document.addEventListener('keypress', function(e){
 );
 
 const box_search = document.getElementById("search")
+const content = document.getElementById("content")
 function searching(question){
     if (window.getComputedStyle(box_search).display !== "none"){
         alert(question)
     }
-     
  }
+
+
+
+
+function visible_search(){
+    if (window.getComputedStyle(box_search).display == "none"){
+        content.style.display = "none"
+        box_search.style.display = "block"
+
+    }else{
+        content.style.display = "block"
+        box_search.style.display = "none"
+    }
+ }
+
+ 
